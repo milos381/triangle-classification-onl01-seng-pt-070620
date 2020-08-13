b302a7f5389b448f29ca18f51c04d97e9f9b9c0a
Milos Popovic <milos_popovic@outlook.com>
2020-08-13 01:57:17 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,28 +1,48 @@
+# class Triangle
+#   attr_accessor :length_one, :lenght_two, :lenght_three
+#
+#   def initialize(length_one, lenght_two, lenght_three)
+#     @length_one = length_one
+#     @lenght_two = lenght_two
+#     @lenght_three = lenght_three
+#     @lenghts = [@length_one, @lenght_two, @lenght_three]
+#
+#   end
+#
+#   def kind
+# <<<<<<< HEAD
+#     if @lenghts.any?{|side| side <= 0 } || ((@lenghts[0] + @lenghts[1]) <= @lenghts[2] || (@lenghts[0] + @lenghts[2]) <= @lenghts[1] || (@lenghts[2] + @lenghts[1]) <= @lenghts[0])
+# =======
+#     if @lenghts.any?{|side| side <= 0 } && ((@lenghts[0] + @lenghts[1]) < @lenghts[2] || (@lenghts[0] + @lenghts[2]) < @lenghts[1] || (@lenghts[2] + @lenghts[1]) < @lenghts[0])
+# >>>>>>> a9a7aa8dac00af4640a284cf685256ab7b52f726
+#       raise TriangleError
+#     elsif length_one == lenght_two && lenght_two == lenght_three
+#       :equilateral
+#     elsif length_one == lenght_two || lenght_two == lenght_three || length_one == lenght_three
+#       :isosceles
+#     else
+#       :scalene
+#
+#     end
+#
+#   end
+#   class TriangleError < StandardError
+#
+#   end
+# end
+
 class Triangle
-  attr_accessor :length_one, :lenght_two, :lenght_three
 
-  def initialize(length_one, lenght_two, lenght_three)
-    @length_one = length_one
-    @lenght_two = lenght_two
-    @lenght_three = lenght_three
-    @lenghts = [@length_one, @lenght_two, @lenght_three]
+  attr_accessor :triangle_one, :triangle_two, :triangle_three
+  def initialize(triangle_one, triangle_two, triangle_three)
+    @triangle_one = triangle_one
+    @triangle_two = triangle_two
+    @triangle_three = triangle_three
+    @lenghts = [@triangle_one, @triangle_two, @triangle_three]
+  end
+
+  def kind(one, two, three)
 
   end
 
-  def kind
-    if @lenghts.any?{|side| side <= 0 } || ((@lenghts[0] + @lenghts[1]) <= @lenghts[2] || (@lenghts[0] + @lenghts[2]) <= @lenghts[1] || (@lenghts[2] + @lenghts[1]) <= @lenghts[0])
-      raise TriangleError
-    elsif length_one == lenght_two && lenght_two == lenght_three
-      :equilateral
-    elsif length_one == lenght_two || lenght_two == lenght_three || length_one == lenght_three
-      :isosceles
-    else
-      :scalene
-
-    end
-
-  end
-  class TriangleError < StandardError
-
-  end
 end
